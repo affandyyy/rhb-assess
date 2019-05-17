0a1404320328c7109e7a5cba312d183188bb4c5f
affandyyy <syahmiaffandy@gmail.com>
2019-05-17 09:46:42 +0800

    Commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+// import ReactDOM from 'react-dom'
+import { BrowserRouter, Route } from 'react-router-dom'
+import Search from './components/Search'
+
+export default class App extends Component {
+  render () {
+    return (
+      <BrowserRouter>
+        <Route 
+            exact path='/' 
+            component={Search}
+        />
+      </BrowserRouter>
+    )
+  }
+}
